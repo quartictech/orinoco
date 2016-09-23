4933e8fa343fa9403d043674c204e640fcc094d5
Alex Sparrow <alex@quartic.io>
2016-09-23 16:56:41 +0000

    csv reading the tracked position data.

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask import request
+import csv
+import geojson
 app = Flask(__name__)
+
+def prepare_geojson(values):
+    pass
 
 @app.route('/gps', methods=['POST'])
 def post_data():
-    print(request.data)
+    data = request.data.decode('utf-8')
+    test = csv.reader(data.split('\n'))
+    for r in test:
+        print(r)
     return ('', 200)
