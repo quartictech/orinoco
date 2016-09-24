3873e6b6582850c2b440f9941cde6954b3150413
Alex Sparrow <alex@quartic.io>
2016-09-24 19:20:18 +0000

    Really WTF

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 import requests
+from pprint import pprint
 import csv
 import json
 import geojson
@@ -13,29 +15,63 @@ layerId = r.json()
 
 app = Flask(__name__)
 
-def prepare_geojson(values):
-    loc = geojson.Point([float(values[4]), float(values[3])])
-    p = {'time' : int(values[0]), 'pid' : values[1], 'timestamp' : round(float(values[2])),
-            'speed' : float(values[5]), 'accuracy' : float(values[6]), 'vaccuracy' : float(values[7]),
-            'battery' : float(values[8]), 'altitude' : float(values[9])}
-    f = geojson.Feature(geometry=loc, properties=p, id=values[1])
-    return geojson.FeatureCollection([f])
+def prepare_geojson(value_list):
+    feature_list = []
+    for values in value_list:
+        loc = geojson.Point([float(values[4]), float(values[3])])
+        p = {'time' : int(values[0]), 'pid' : values[1], 'timestamp' : round(float(values[2])),
+                'speed' : float(values[5]), 'accuracy' : float(values[6]), 'vaccuracy' : float(values[7]),
+                'battery' : float(values[8]), 'altitude' : float(values[9])}
+        f = geojson.Feature(geometry=loc, properties=p, id=values[1])
+        feature_list.append(f)
+    return geojson.FeatureCollection(feature_list)
 
 @app.route('/gps', methods=['POST'])
 def post_data():
     data = request.data.decode('utf-8')
     test = csv.reader(data.split('\n'))
+
     next(test)
+
+    features = []
+
+    point_ids = []
+    trip_id = ''
+
     for r in test:
+        pprint(r)
+
         if (len(r) == 0):
             continue
-        print(r)
         #TIME POINTID DATE LATITUDE LONGITUDE SPEED HACCU VACCU BAT ALTITUDE
         pos_info = (r[8], r[3], r[12], r[13], r[14], r[15], r[17], r[18], r[19], r[20])
-        geoj = prepare_geojson(pos_info)
-        print(geoj)
+        point_ids.append(int(r[11]))
+        trip_id=int(r[5])
+        features.append(pos_info)
 
-        r = requests.post("{}/layer/live/{}".format(apiRoot, layerId), json=geoj)
-        print(r)
+    geoj = prepare_geojson(features)
 
-    return ('', 200)
+    pprint(geoj)
+
+    r = requests.post("{}/layer/live/{}".format(apiRoot, layerId), json=geoj)
+    print(r)
+
+    response = {
+            'id': 0,
+            'tripid': trip_id,
+            'points': point_ids,
+            'valid': True
+            }
+
+    print(json.dumps(response))
+            
+
+    stuff = jsonify(
+            id=0,
+            tripid=trip_id,
+            points=point_ids,
+            valid=True)
+
+    pprint(stuff)
+
+    return stuff
