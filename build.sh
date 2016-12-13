4515ae3d1f92d4bdc72fa1f68664587b78afa77b
Alex Sparrow <alex@quartic.io>
2016-12-13 17:29:50 +0000

    Hopefully fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ QUARTIC_DOCKER_REPOSITORY=${QUARTIC_DOCKER_REPOSITORY-quartic}
 
 docker build -t orinoco .
 
-function build {
+function build() {
   docker build -t $QUARTIC_DOCKER_REPOSITORY/$1:$VERSION ./$1
   docker push $QUARTIC_DOCKER_REPOSITORY/31:$VERSION
 }
