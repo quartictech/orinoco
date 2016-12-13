fbb45fd8456de2370f1267762542ed23875f9878
Alex Sparrow <alex@quartic.io>
2016-12-13 17:15:13 +0000

    Remove version

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 VERSION=${CIRCLE_BUILD_NUM-Unknown}
 QUARTIC_DOCKER_REPOSITORY=${QUARTIC_DOCKER_REPOSITORY-quartic}
 
-docker build -t orinoco:$VERSION .
+docker build -t orinoco .
 docker build -t $QUARTIC_DOCKER_REPOSITORY/national-grid:$VERSION ./national-grid
 
 docker push $QUARTIC_DOCKER_REPOSITORY/national-grid:$VERSION
