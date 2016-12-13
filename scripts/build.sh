5c7758865fefb36884f8a3dc2f762da139badd50
Alex Sparrow <alex@quartic.io>
2016-12-13 17:12:07 +0000

    Attempt build fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 VERSION=${CIRCLE_BUILD_NUM-Unknown}
 QUARTIC_DOCKER_REPOSITORY=${QUARTIC_DOCKER_REPOSITORY-quartic}
 
-docker build -t $QUARTIC_DOCKER_REPOSITORY/orinoco:$VERSION .
+docker build -t orinoco:$VERSION .
 docker build -t $QUARTIC_DOCKER_REPOSITORY/national-grid:$VERSION ./national-grid
 
-docker push $QUARTIC_DOCKER_REPOSITORY/orinoco:$VERSION
+docker push $QUARTIC_DOCKER_REPOSITORY/national-grid:$VERSION
