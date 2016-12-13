2ff778d8fdde05ea13f8254159952cea72355222
Alex Sparrow <alex@quartic.io>
2016-12-13 17:26:54 +0000

    More builds

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+VERSION=${CIRCLE_BUILD_NUM-Unknown}
+QUARTIC_DOCKER_REPOSITORY=${QUARTIC_DOCKER_REPOSITORY-quartic}
+
+docker build -t orinoco .
+
+function build {
+  docker build -t $QUARTIC_DOCKER_REPOSITORY/$1:$VERSION ./$1
+  docker push $QUARTIC_DOCKER_REPOSITORY/31:$VERSION
+}
+
+build national-grid
+build tracker
+build tayo
