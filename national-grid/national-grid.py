6718b9b95bd8935f588ad1e685b83b8094ff9ef8
Alex Sparrow <alex@quartic.io>
2016-12-13 14:10:43 +0000

    Port national-grid to orinoco

--- national-grid/national-grid.py
+++ national-grid/national-grid.py
@@ -1,24 +1,20 @@
 from utils import fetch_data
 from collections import defaultdict
 from pprint import pprint
-from aiohttp import web
 import asyncio
 import json
 from datetime import datetime
 import logging
+import os.path
+
+from orinoco import create_app
+
 logging.basicConfig(level=logging.INFO, format='%(levelname)s [%(asctime)s] %(name)s: %(message)s')
 
-places = json.load(open("data/places.json.geocoded"))
+data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "places.json.geocoded")
+
+places = json.load(open(data_path))
 flows = defaultdict(dict)
-
-async def status(request):
-    return web.Response(text="Sweet")
-
-websockets = set()
-
-async def send_event(event):
-    for ws in websockets:
-        ws.send_str(json.dumps(event))
 
 def convert_time_series(d):
     for k, v in d.items():
@@ -27,7 +23,7 @@ def convert_time_series(d):
             "value": v
         }
 
-async def send_time_series(key, data):
+def send_time_series(key, data):
     if not key in places:
         logging.warn("key not found: " + key)
         return
@@ -51,13 +47,12 @@ async def send_time_series(key, data):
         },
         "timestamp": 0
     }
-    await send_event(message)
+    return message
 
-async def main_loop(app):
+async def main_loop():
     await asyncio.sleep(5)
     while True:
         logging.info("processing...")
-        logging.info("open websockets: %d", len(websockets))
         result = fetch_data()
         for key, data in result["data"].items():
             if key == "TOTAL SUPPLY": continue
@@ -70,41 +65,10 @@ async def main_loop(app):
                     time_series[row['applicable_at']] = row['flow_rate']
                     dirty = True
             if dirty:
-                await send_time_series(key, time_series)
+                yield send_time_series(key, time_series)
 
         await asyncio.sleep(10)
 
-async def websocket_handler(request):
-    logging.info("Registering websocket connection")
-    global websockets
-
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-    websockets.add(ws)
-
-    # Send all data on startup
-    logging.info("sending initial time series")
-    for key, time_series in flows.items():
-        await send_time_series(key, time_series)
-
-    async for msg in ws:
-        pass
-
-    logging.info("Unregistering websocket connection")
-    websockets.remove(ws)
-
-    return ws
-
-async def start_background_tasks(app):
-    app['main_loop'] = app.loop.create_task(main_loop(app))
-
-async def cleanup_background_tasks(app):
-    app['main_loop'].cancel()
-
 if __name__ == "__main__":
-    app = web.Application()
-    app.on_startup.append(start_background_tasks)
-    app.on_cleanup.append(cleanup_background_tasks)
-    app.router.add_get('/healthcheck', status)
-    app.router.add_get('/national-grid', websocket_handler)
-    web.run_app(app)
+    app = create_app("national-grid", main_loop)
+    app.run()
